92feab7f2fbfe7706baf293cad5b6737a2d2ac81
Juan Cavalcante <107267670+juanvictorBC@users.noreply.github.com>
2023-11-28 20:16:03 -0300

    implemetation methods

--- src/model/entities/Worker.java
+++ src/model/entities/Worker.java
@@ -1,6 +1,7 @@
 package model.entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import model.entities.enums.WorkerLevel;
@@ -10,11 +11,11 @@ public class Worker {
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
-	
-	//compositions
+
+	// compositions
 	private Department department;
 	List<HourContract> contracts = new ArrayList<HourContract>();
-	
+
 	public Worker() {
 	}
 
@@ -61,4 +62,25 @@ public class Worker {
 		return contracts;
 	}
 
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+  		}
+		return sum;
+	}
 }
